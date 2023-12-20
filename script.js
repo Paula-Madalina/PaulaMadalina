d3c5a7d0dbdb37b71ca0619d3110beff7d46d538
Paula-Madalina <prepemadalinutza@yahoo.com>
2023-12-20 17:04:34 +0200

    bvgfh

---
+++ script.js
@@ -0,0 +1,253 @@
+// console.log("Prepelita Paula-Madalina");
+// alert("ALERTA!!!")
+
+// let output=document.getElementById("output");
+// output.innerText="Aici este un text"
+// output.style.color = "red";
+
+
+// VARIABILE
+// let text;
+//  console.log(text);
+//  text="jhshshs";
+// console.log(text);
+
+//  TIPURI DE DATE
+// String aka text
+//  let text = "ushnfnhs";
+
+
+// Number numar
+// let number=100;
+
+// boolean true/false
+// let boolean=true;
+// let boolean=false;
+
+// let a=10;
+// let b=3;
+// let c= a%b;
+// let d= true;
+
+// a=a+1;
+// a++;
+// a--;
+
+// a=a+120;
+// a+=120;
+
+// a-=120;
+// a*=10;
+// a/=10;
+//console.log(a);
+// console.log(b);
+// console.log(a+b);
+//console.log(c);
+
+
+// for(let i=0; i<100; i++)
+// {
+// console.log(i);
+// }
+
+// console.log("Text1:" + c+a+b);
+// console.log(`Text : ${c} si ${a} si ${b}`);
+// let test = typeof(d);
+// console.log(test);
+
+
+// let a=5;
+// a=a.toString()
+// let b=parseInt("4");
+// console.log(a+b);
+
+// EX. 
+
+// let age = 11;
+// console.log(age);
+// age+=1;
+// console.log(age);
+// let name= "Paula";
+// console.log(name);
+
+// console.log(typeof(name));
+
+// age*=2;
+// console.log(age);
+// age/=3;
+// console.log(age);
+
+
+// FUNCTII
+
+// let valoare = prompt();
+// console.log(valoare); 
+// document.getElementById
+// obiect.metoda.valoare
+
+// function printName() {
+//     let userInput=document.getElementById("name_input").value;
+//     console.log(userInput);
+// }  
+
+// let suma=sum(5,4);
+// console.log(suma);
+
+// function sum(a,b) 
+// {
+//     return a+b;
+// }
+
+// EXERSARE
+// document.write("HELLO WORLD!");
+
+// document.getElementById("paragraph").innerText = "Heyyyyy";
+
+// // alert("hellooooooo");
+
+// console.log("EXEMPLU")
+
+
+// var tutorial = 25;
+// var Tutorial = 50;
+// document.write(tutorial);
+
+// var pisica = {
+//     culoare: "alba",
+//     varsta: "2ani"
+
+// }
+
+// console.log(pisica.culoare)
+
+// var numar = 10;
+// numar-=5;
+// numar+=5
+// numar*=2;
+// numar--
+// console.log(numar);
+
+
+// 3 variabile, string, numar nuamr
+
+// let test = "George";
+// let a=10;
+// let b= 20;
+// console.log(a*b);
+
+// let a=12;
+// let b=23;
+// console.log(b%2);
+
+// console.log("Prima valoare este: " + a%2 + " a doua valoare este: " + b%2);
+// console.log(`Prima valoare este: ${a%2}\n a doua valoare este: ${b%2}`);
+
+// let exemple = document.getElementById("momentan");
+// exemple.innerHTML = `<p id="p1">Aici este un paragraf</p>`;
+
+// let test = document.getElementById("p1");
+// test.innerText = `Aici este alt text`;
+
+
+
+
+// exemple.style.width = "200px";
+// exemple.style.height = "200px";
+// exemple.style.backgroundColor = "red";
+
+
+// function printName() {
+//     console.log("ceva");
+//     console.log (sum(2, 5, 6));
+
+// }
+
+// function sum(a, b, c) {
+//     return a + b + c;
+// }
+
+
+function printName() {
+    let a = document.getElementById("name_input").value;
+    let b = document.getElementById("name_email").value;
+    let div = document.getElementById("div1");
+    let c
+}
+
+    // if(div.style.width=="200px")
+    // {
+    //     div.style.width= "600px"
+    // }
+    // else 
+    // {
+    //     div.style.width= "200px"
+    // }
+    
+
+
+
+// if (input_name == "Paula") 
+// {
+//     alert("Helloo Paula");
+// }
+// if(input_name!="Paula") 
+// {
+//     alert("Helloo someone else");
+// }
+
+
+// if (a>b)
+// {
+//     alert(`${a} este mai mare ca ${b}`)
+// }
+// else if(a==b)
+// {
+//     alert(`${a} este egal ${b}`)
+// }
+// else {
+//     alert(`${a} este mai mic ca ${b}`)
+    
+// }
+
+// }
+
+
+
+
+
+
+
+// IF ELSE
+// let a=4;
+// if (a<5)
+// {
+//     console.log("a este mai mic decat 5");
+// }
+// console.log(a);
+
+// // IF
+
+// if(a!=5)
+// {
+
+// }
+//  
+
+// function printName() 
+// {
+//     let div1 = document.getElementById("div1")
+//     let div2 = document.getElementById("div2")
+//     let div3 = document.getElementById("div3")
+//     console.log("Hello")
+//     if(div1.style.backgroundColor=="aqua" && div2.style.backgroundColor=="red" || div3.style.backgroundColor=="gold")
+//     {
+//         div1.innerText= div1.style.backgroundColor
+//         div2.innerText= div2.style.backgroundColor
+//         div3.innerText= div3.style.backgroundColor
+
+//     }
+
+
+// }
+
+
