b12800f423b5fd19f89c8b2eeb704371e4a5f5b2
Paula-Madalina <prepemadalinutza@yahoo.com>
2024-01-18 15:58:18 +0200

    from PC

--- script.js
+++ script.js
@@ -167,12 +167,12 @@
 // }
 
 
-function printName() {
-    let a = document.getElementById("name_input").value;
-    let b = document.getElementById("name_email").value;
-    let div = document.getElementById("div1");
-    let c
-}
+// function printName() {
+//     let a = document.getElementById("name_input").value;
+//     let b = document.getElementById("name_email").value;
+//     let div = document.getElementById("div1");
+//     let c
+// }
 
     // if(div.style.width=="200px")
     // {
@@ -250,4 +250,411 @@ function printName() {
 
 // }
 
+// function Hello() {
+    // let string= "";
+    // for (let i=0; i<10; i++) {
+    //     for (let j=0; j<10; j++) {
+    //     string+= "*";
 
+    //  }
+
+//     let result=" ";
+//     for(let i=0; i<=10;i++) {
+//         for(let j=1; j<=10;j++) {
+//             result += i*j + ", ";
+//         }
+//         result+="\n";
+        
+//     }
+//     document.body.innerText=result;
+// }
+// 1, 2,3,.......
+// 2,4,6,........
+
+// let colors = ["red", "green", "blue"];
+// let numbers = [0, 1, 2, 3, 4, 5, 6, 7];
+// let aa =["a", 1, "b", 2];
+
+
+// // for(let i = 0; i < colors.length; i++) {
+// //     console.log(colors[i]);
+// // }
+
+// for(let element of numbers) {
+//     console.log(element);
+// }
+
+
+
+// let dogs = [];
+// dogs[0] = "pitbull";
+// dogs[1] = "bulldog";
+// console.log(dogs);
+
+
+// console.log(colors[0]);
+// console.log(colors.length);
+
+
+// let evenNr = [2,4,6,8,10];
+
+// // // for(let i=0; i>0; i++) {
+// // //     evenNr[i] = evenNr[i] * 2;
+// // //     console.log(evenNr[i]);
+// // // }
+
+
+// // // SCRIS INVERS
+// for(let i=evenNr.length-1; i>=0; i--) {
+//     console.log(evenNr[i]);
+// }
+
+// let colors = ["red", "green", "blue"];
+// let numbers = [0, 1, 2, 3, 4, 5, 6, 7];
+// let aa =["a", 1, "b", 2];
+// let test = [5,9,2,1,88];
+
+
+// SORT (arrow function)
+// test.sort((a,b) => {return a-b});
+// console.log(test);
+
+
+
+
+// CONCAT(CONCATENARE) ARRAY
+
+// let all = colors.concat(numbers);
+// console.log(all);
+
+
+// // JOIN
+// let sentence = ["Hello", "world"];
+// let text = sentence.join(" ");
+// console.log(text);
+
+// SPLIT
+let text2= "Afara ninge";
+let arr = text2.split("").reverse().join(" ");
+console.log(arr);
+
+// Scoaterea ultimului element
+// numbers.pop();
+// console.log(numbers);
+
+// // ADAUGAREA DUPA UN ULTIM ELEMENT
+// numbers.push(1);
+// console.log(numbers);
+
+// ADAUGAREA UNUI ELEMENT LA INCEPUT
+
+// unshift
+
+// SCOATEREA PRIMULU ELEMENT
+// console.log(colors.shift());
+// console.log(colors);
+
+// REVERSE ELEMENTE
+
+// let numbers2= numbers.reverse();   
+// console.log(numbers2);
+
+
+// INDEX OF
+// let indexOfBlue= colors.indexOf("blue");
+// console.log(indexOfBlue);
+
+// // INCLUDES
+// let bbb =  colors.includes("blue");
+// console.log(bbb);
+
+// SPLICE 
+// colors.splice(0,1, "hello");
+// console.log(colors);
+
+// 
+// let colors2= colors.slice(0,2);
+// console.log(colors2);
+
+
+// SORTARE ASCENDENTA(cu SPOT(a-b))
+// let arr= [4,7,2];
+// arr.sort((a,b) => a-b);
+// console.log(arr);
+
+// // SORTARE DESCENDENTA(cu SCHIMBAREA PARAMETRILOR(b-a))
+// arr.sort((a,b) => b-a);
+// console.log(arr);
+
+
+// EX
+
+// let rezultate= [];
+// for(let i=1; i<=5; i++) {
+//     rezultate.push(prompt());
+// }
+// console.log(rezultate);
+
+
+// Tema de la practice-home practice de la for si de la array
+
+// FOR LOOPS
+// EX 1
+// Write a program that picks a number and shows all the small positive numbers
+// smaller than it.
+
+// 
+// EX 4 
+// 1 medoda: sort
+// 2 metoda sort
+
+// let numere = [];
+// for (let i =0; i < 10; i++) {
+//     numere.push(Math.floor(Math.random() * 1000+1));
+// }
+//     numere.sort((a,b) => a-b);
+//     console.log(numere);
+//     console.log(numere[numere.length - 1]);
+
+
+// let maxNr= 0;
+// for(let i=0; i<10; i++) {
+//     let nrCurent= Math.floor(Math.random() *1000+1);
+//     if(maxNr<nrCurent) {
+//         maxNr=nrCurent;
+//     }
+// }
+// console.log(maxNr)
+
+
+// let maxNr= 1;
+// let currentNr= [1,3,5,8,99,101];
+// for(let i=0; i<currentNr.length; i++) {
+//     let currentNr2= currentNr[i];
+//     if(maxNr < currentNr2)
+//     {
+//         maxNr = currentNr2;
+//     }
+    
+// }
+// console.log(maxNr);
+
+// ARRAY
+// EX 1
+
+// let arr= [4,6,10,12];
+// console.log(arr[1]);
+
+// // EX 2
+// ~
+// let arr2 = [4,20,30,10];
+// console.log(arr2[arr2.length-1]);
+
+// // EX 3
+// let arr3 = [4,20,30,10,20];
+// let middleNr= Math.floor(arr3.length / 2);
+// console.log(arr3[middleNr]);
+
+
+// // EX 4
+// let arr4 = [23,22,30,11]
+// if(arr[arr4.length-1] == arr[0]) {
+//     console.log("YES");
+// } else {
+//     console.log("NO");
+// }
+
+// // EX 5 
+// // let arr5 = [23,5,30,11,12];
+// // let minNr = Math.min(...arr5);
+// // console.log(minNr);
+
+// let arr5= [23,5,30,11,12];
+// let minNr = arr5[0];
+
+// for (let i=1; i<arr5.length; i++) {
+//     if(arr5[i] < minNr) {
+//         minNr = arr5[i];
+// } 
+// }
+// console.log(minNr);
+
+// EX 6 
+// Hello()
+// function Hello() {
+//     let arr7 = [];
+//     for (let i = 0; i < 5; i++) {
+//         let input = prompt("Enter a number:");
+//         let number = parseFloat(input);
+//         arr7.push(number);
+//     }
+//     console.log(arr7);
+//     let maxNr = Math.max(...arr7);
+//     document.getElementById("max").innerText = `Max Nr: ${maxNr}`;
+
+
+// }
+
+// Hello();
+// function Hello() {
+    
+//     let arr = [];
+//     for (let i = 0; i < 5; i++) {
+//         // let input = prompt("Enter a number:");
+//         // let number = parseFloat(input);
+//             arr.push(prompt());
+
+        
+//         // if (!isNaN(number)) {
+//         //     arr.push(number);
+//         // } else {
+//         //     alert("Invalid input. Please enter a valid number.");
+//         //     i--;
+//         // }
+//     }
+//     const maxNumber = Math.max(...arr);
+//     document.getElementById("max").innerText = `The max number is:  ${maxNumber}`;
+//     console.log(arr);
+// }
+
+
+// // ex MATH
+// let numar = 12.1736824931;
+// // MATH
+
+// console.log(numar.toFixed(2));
+
+// console.log(Math.E, Math.PI);
+
+// // abs
+// console.log(Math.abs(12.3), Math.abs(-12.4));
+
+// // ceil- ne da nr intreg mai mare decat nr scris 
+// console.log(Math.ceil(12.3));
+
+// // floor- ne da nr intreg mai mic decat nr
+// console.log(Math.floor(12.3));
+
+// // exp- il ridica pe E la exponentul din paranteza;
+// console.log(Math.exp(1));
+
+// // pow- ridicarea la putere
+// console.log(Math.pow(2,2), Math.pow(2,4), Math.pow(81,0.5));
+
+// // sqrt-afla radacina patrata din paranteza
+// console.log(Math.sqrt(64));
+
+// // round-rotunjeste la cel mai apropiat nr intreg
+
+// console.log(Math.round(6.521));
+
+// // random-genereaza un nr aleator
+
+// let min= 1;
+// let max=10;
+// console.log(Math.floor(min + Math.random()*max));
+
+// // min
+// console.log(Math.min(1,-3,7), Math.max(6,8,3));
+
+// CONVERSIA DATELOR
+
+// let a= "16";
+// let b=8;
+// console.log(b-"trei");
+// let val = b-a;
+// console.log(isNaN(val));
+
+// conversii explicite
+// 1.conversia de la string la number
+// parseInt, parseFloat, Number
+
+// a= parseInt("8");
+// b= parseFloat("2.5");
+// console.log(a+b);
+// let c= Number("25");
+// console.log(c);
+
+// // 2. conversii la tipul string
+// // toStrig , String
+// let rez= String(123);
+// rez= String(5.3+2)
+// console.log(rez, typeof rez);
+// let sss= (123).toString();
+// console.log(sss, typeof sss);
+
+// // conversii la boolean
+// let result= Boolean("");
+// console.log(result, typeof result);
+// result = Boolean(0);
+// console.log(result, typeof result);
+
+
+// CODE WARS
+
+// let arr =[];
+
+// function test(a,b){
+// let randomNr= Math.floor(Math.random());
+// for(let i=1; i<=2; i++){
+//     let input = prompt("Please enter a random number")
+//     let number = parseFloat(input);
+//     let sum= a+b;
+//     arr.push(number);
+//     if(number==100 || sum==100) {
+//         return true;
+//     }
+//     return false;
+    
+// }
+// console.log(number);
+// }
+
+// test();
+
+
+// function test(a, b) {
+//     let arr = [];
+//     for (let i = 1; i <= 2; i++) {
+//         let input = prompt("Please enter a random number");
+//         let number = parseFloat(input);
+//         arr.push(number);
+//     }
+
+//     let sum = arr[0] + arr[1];
+    
+//     if (arr[0] === 100 || arr[1] === 100 || sum === 100) {
+//         console.log(true);
+//         console.log("Selected numbers: ", arr.join(', '));
+        
+        
+//     } else {
+//         console.log(false);
+//         console.log("Selected Numbers:", arr.join(', '));
+        
+
+//     }
+
+    
+// }
+
+// console.log(test());
+
+
+// function test() {
+//     let arr = [];
+//     for (let i = 1; i <= 2; i++) { 
+//         let input = prompt("Please enter a number");
+//         let number = parseInt(input);
+//         arr.push(number);
+//     }
+//     let sum= arr[0] + arr[1];
+//     if (arr[0] === 100 || arr[1] === 100 || sum ===100) {
+//         console.log(`Selected numbers are: ${arr[0]} and ${arr[1]}`);
+//         return true;
+//     } else {
+//         console.log(`Selected numbers are: ${arr[0]} and ${arr[1]}`);
+//         return false;
+//     }
+// }
+// console.log(test());1
